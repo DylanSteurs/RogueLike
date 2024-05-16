77165bbe84eebf000909ef09e6f7d00d8128fd99
DylanSteurs <144011463+DylanSteurs@users.noreply.github.com>
2024-05-16 11:45:52 +0200

    Enemies added
    Enemies zijn toegevoegd, maar ze lopen natuurlijk nog niet

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -44,10 +44,15 @@ public class MapManager : MonoBehaviour
     public int roomMaxSize = 10;
     public int roomMinSize = 6;
     public int maxRooms = 30;
-
+    public int maxEnemies = 2;
     private void Start()
     {
         GenerateDungeon();
+        SetMaxEnemies(maxEnemies);
+    }
+    private void SetMaxEnemies(int max)
+    {
+        maxEnemies = max;
     }
 
     private void GenerateDungeon()
@@ -65,14 +70,6 @@ public class MapManager : MonoBehaviour
         AddTileMapToDictionary(ObstacleMap);
         SetupFogMap();
     }
-
-    public GameObject CreateActor(string name, Vector2 position)
-    {
-        GameObject actor = Instantiate(Resources.Load<GameObject>($"Prefabs/{name}"), new Vector3(position.x + 0.5f, position.y + 0.5f, 0), Quaternion.identity);
-        actor.name = name;
-        return actor;
-    }
-
     public bool InBounds(int x, int y) => 0 <= x && x < width && 0 <= y && y < height;
 
     public bool IsWalkable(Vector3 position)
